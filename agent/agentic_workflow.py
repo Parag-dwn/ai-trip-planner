1ef96f2a6fc25248ce6193929f02c809de372140
Parag-dwn <paragdewangan1@gmail.com>
2026-04-26 12:14:42 +0530

    code updated for loading the model

---
+++ agent/agentic_workflow.py
@@ -0,0 +1,21 @@
+from utils.model_loader import ModelLoader
+from tools.weather_info_tool import WeatherInfoTool
+from tools.place_Search_tool import PlaceSearchTool
+from tools.expense_calculator_tool import CalculatorTool
+from tools.Currency_conversion_tool import CuurrencyConverterTool
+from prompt_library.prompt import SYSTEM_PROMPT
+from langgraph.graph import StateGraph, MessageState,END,START
+
+class GraphBuilder():
+
+    def __init__(self):
+        pass
+
+    def agent_function(self):
+        pass
+    
+    def build_graph(self):
+        pass
+    
+    def __call__(self):
+        pass
